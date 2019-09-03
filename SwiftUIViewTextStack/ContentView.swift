a6e6e5ce58e7fab7f2f03fe612024ae244b293d1
Alex Nagy <support@rebeloper.com>
2019-09-03 16:07:01 +0300

    first commit

--- SwiftUIViewTextStack/ContentView.swift
+++ SwiftUIViewTextStack/ContentView.swift
@@ -9,13 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var text1 = "Hello Text 1"
+    var text2 = "Hello Text 2"
+    var text3 = "Hello Text 3"
+    
     var body: some View {
-        Text("Hello World")
+        VStack() {
+            HelloView(textString: text1, font: .title, foregroundColor: .blue)
+            HStack (spacing: 50) {
+                HelloView(textString: text2, font: .subheadline, foregroundColor: .yellow)
+                HelloView(textString: text3, font: .subheadline, foregroundColor: .red)
+            }
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HelloView: View {
+    
+    var textString: String
+    var font: Font
+    var foregroundColor: Color
+    
+    var body: some View {
+        Text(textString)
+            .font(font)
+            .fontWeight(.bold)
+            .foregroundColor(foregroundColor)
     }
 }
